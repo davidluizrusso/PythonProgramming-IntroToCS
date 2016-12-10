619966ac2894fb87ecdaa97a7ed09236fc173e22
davidluizrusso <david.luiz.russo@gmail.com>
2016-12-10 10:29:20 -0800

    Added second solution

--- Chapter2/chapter2_exercises.py
+++ Chapter2/chapter2_exercises.py
@@ -15,3 +15,20 @@ def main():
 
 main()
 
+#2. Modify the avg2.py program to find the average of three exam scores.
+
+def main():
+    print("This program comptues the average of three exam scores.")
+    
+    score1, score2, score3 = eval(input(
+    "Enter three scores separated by a comma: "
+                                        ))
+    average = (score1 + score2 + score3) / 3
+    
+    print("The average of the scores is:", average)
+
+main()
+
+#3. 
+    
+    
