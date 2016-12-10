2497d3335448c9075aab30d39ee4204546ff7cbf
davidluizrusso <david.luiz.russo@gmail.com>
2016-12-10 10:19:49 -0800

    First exercise in chapter 2

--- Chapter2/chapter2_exercises.py
+++ Chapter2/chapter2_exercises.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Solutions to Programming Exercises for Chapter 2
 """
+
+# 1. Modify the convert.py program to print an introduction
+
+def main():
+    print("This program will convert Celsius to Fahrenheit.")
+    print("You will be prompted to enter the temperature in Celsius.")
+    print("Type in the temperature and press enter.")
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+main()
 
